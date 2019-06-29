0323dd3fb5e3b5e0fdd1626e42fd2ddfba7ed9b1
divi15987 <divi15987@gmail.com>
2019-06-29 13:23:42 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const app=express();
+const http=require("http")
+const server=http.createServer(app)
+const io=require("socket.io")(server)
+
+const port=process.env.PORT||3030;
+//Static folder to keep all static files
+app.use(express.static(__dirname+"/public"))
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html")
+})
+
+io.on("connection",(client)=>{
+    console.log("Connected.......");
+    client.emit("acknowledge",{message:"Connection Established"})
+    client.on("MsgToServer",(chatterName,message)=>{
+        console.log(chatterName+" says:"+message);
+        client.emit("MsgToClient",'Me',message);
+    client.broadcast.emit("MsgToClient",chatterName,message)
+}) 
+})
+server.listen(port,()=>{
+    console.log("Socket Server has started"+port);
+    
+})
